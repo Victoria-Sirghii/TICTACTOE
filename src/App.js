20b3492fe15524fbf72f769a6d7045dd292d044a
Victoria <vika.sirghii@gmail.com>
2021-03-10 22:33:14 +0200

    transform game history

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './helpers';
 
 import './styles/root.scss'
 
 function App() {
+  const [ history, setHistory ] = useState([
+    {board: Array(9).fill(null), isXNext : true}
+  ]);
+  const [ currentMove, setCurrentMove  ] = useState(0);
+  const current = history[currentMove];
+
+  const winner = calculateWinner(current.board);
+  const message = winner 
+    ? `Winner is ${winner}`
+    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClcik = position => {
+      if(current.board[position] || winner){
+          return;
+      }
+
+      setHistory(prev => {
+        const last = prev[prev.length - 1];
+
+        const newBoard =  last.board.map((square, pos) => {
+              if(pos === position) {
+                  return last.isXNext ? 'X' : 'O';
+              }
+              return square;
+          });
+          return prev.concat({ board: newBoard, isXNext: !last.isXNext})
+      });
+      setCurrentMove(prev => prev + 1)
+
+  };
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <h1>{ message }</h1>
+      <Board board={current.board} handleSquareClcik={handleSquareClcik} />
     </div>
   );
 }
