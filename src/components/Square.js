f10e84d1465e7799ef6729249307d0c2b65ed090
Victoria <vika.sirghii@gmail.com>
2021-03-11 22:30:39 +0200

    project finish deployement

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,8 +4,8 @@ import React from 'react';
 const Square = ({ value, onClick, isWinningSquare }) => {
     return (  
         <button 
-            className="square" 
-            style={{fontWeight: isWinningSquare ? 'bold' : 'normal'}}
+            className={`square ${isWinningSquare ? 'winning' : ""}
+                ${value === 'X' ? 'text-green' : 'text-orange'}`}
             type="button"
             onClick={onClick}>
             {value}
